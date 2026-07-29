08ffc9b61178f1d39c89bc33f38ae4a8c0a1f3a2
dh <daehyunazenith@gmail.com>
2026-07-29 20:43:37 +0900

    feat:첫 번째 파이썬 코드  gitignoer 추가

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+data={
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Score': [90, 85, 95]
+}
+
+df=pd.DataFrame(data)
+print("===My First Pandas DataFrame in Linux===")
+print(df)
+
+
+
